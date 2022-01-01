16a505301c2533314065886c8e5dfc96d7e4c258
Samyuktha Rajesh <samyuktha.rajeshd@gmail.com>
2022-01-01 18:26:16 +0530

    Added sign-up page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta charset="utf-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, shrink-to-fit=no"
+				/>
+
+				<meta
+					name="description"
+					content="Tivo is a HTML landing page template built with Bootstrap to help you crate engaging presentations for SaaS apps and convert visitors into users."
+				/>
+				<meta name="author" content="Inovatik" />
+
+				<link
+					href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap&subset=latin-ext"
+					rel="stylesheet"
+				/>
+				<link href="css/bootstrap.css" rel="stylesheet" />
+				<link href="css/fontawesome-all.css" rel="stylesheet" />
+				<link href="css/swiper.css" rel="stylesheet" />
+				<link href="css/magnific-popup.css" rel="stylesheet" />
+				<link href="css/styles.css" rel="stylesheet" />
+
+				<link rel="icon" href="images/favicon.png" />
+
+				<script src="js/jquery.min.js"></script>
+				<script src="js/popper.min.js"></script>
+				<script src="js/bootstrap.min.js"></script>
+				<script src="js/jquery.easing.min.js"></script>
+				<script src="js/swiper.min.js"></script>
+				<script src="js/jquery.magnific-popup.js"></script>
+				<script src="js/validator.min.js"></script>
+				<script src="js/scripts.js"></script>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
